6522eacdbeb4377c1a37b597565e0f7070e9026f
Lorena Moujalli <lorenamoujalli860@gmail.com>
2026-05-21 16:22:02 -0400

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,19 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
-export default defineConfig({});
+import netlify from '@astrojs/netlify';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react()],
+  output: 'server',
+  adapter: netlify(),
+  
+  // Agrega esta sección de Vite aquí abajo:
+  vite: {
+    preview: {
+      allowedHosts: true
+    }
+  }
+});
